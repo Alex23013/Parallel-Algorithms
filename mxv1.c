a923cd3e022e7149bd1cf9ee2064911f8d990ed2
Maria Cornejo <alecornejolupa2013@gmail.com>
2018-04-17 20:04:44 -0500

    mxv1 almost repair

--- mxv1.c
+++ mxv1.c
@@ -1,88 +1,84 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 #include <mpi.h>
+//Compilar: mpicc -g -Wall -o mpi MPIMultMatrix.c Ejecutar: mpiexec -n 4 ./mpi
+/*Ejecutar: mpiexec -n [num_proc] ./[nombre]*/
+/*Como la matriz [][] -> se verá como vector-Usamos la sngt func*/
+void Read_vector(double local_a[], int local_n, int n, char vec_name[], int my_rank, MPI_Comm comm){
+  double* a = NULL;
+  int i;
 
+  if(my_rank == 0){
+    a = malloc(n*sizeof(double));
+    printf("ENter the vector %s\n", vec_name);
+    for(i = 0; i < n; i++)
+      {scanf("%lf", &a[i]);}
+     MPI_Scatter(a, local_n, MPI_DOUBLE, local_a, local_n, MPI_DOUBLE,0,comm);
+     free(a);
+    
+  }
+    else{
+      MPI_Scatter(a,local_n,MPI_DOUBLE,local_a,local_n,MPI_DOUBLE,0,comm);
+    }
+  
 
-int main(int argc, char* argv[]) {
-
-	int m, n, n_local, i,j,local_m,local_n;
-	double local_start, local_finish, local_elapsed, elapsed;
-	MPI_Comm comm;
-	
-	m=atoi(argv[1]);
-	n=atoi(argv[1]);
-	srand(time(NULL));
-	
-	int v[n], result[n];
-	int *A = (int *)malloc(sizeof(int) * m*n);
-	int rank;
-	int size;
-	printf("Vivo\n");
-        MPI_Init(&argc, &argv);
-	comm = MPI_COMM_WORLD;
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	MPI_Comm_size(MPI_COMM_WORLD, &size);
-	
-	local_m=m/size;
-	local_n=n/size;
-
-	if(rank==0){	
-
-		for(i=0;i<m*n;++i)
-			A[i]=(rand() %100000);
-
-		for(i=0; i<n; ++i){
-			v[i]=(rand() %100000);
-			//printf("%d, ",v[i]);
-		}
-	}
-	MPI_Barrier(comm);
-	local_start=MPI_Wtime();
-	int *local_A = (int *)malloc(sizeof(int) * local_m*n);
-	int *local_x = (int *)malloc(sizeof(int) * local_n);
-	int *local_y = (int *)malloc(sizeof(int) * local_m);
-	int *res = (int *)malloc(sizeof(int) * m);
-	MPI_Scatter(A, local_m*n, MPI_INT, local_A,
-              local_m*n, MPI_INT, 0, comm);
-	MPI_Scatter(v, local_n, MPI_INT, local_x,
-              local_n, MPI_INT, 0, comm);
-	//MPI_Scatter(result, local_m, MPI_INT, local_y,
-              //local_m, MPI_INT, 0, comm);
-	
-	//for(i=0; i<local_m*n; ++i)
-		//printf("Proceso %d, tengo %d\n", rank,local_A[i]);
-	//for(i=0; i<local_n; ++i)
-		//printf("Proceso %d, tengv %d\n", rank,local_x[i]);
-	int *x=(int*)malloc(n*sizeof(int));
-	MPI_Allgather(local_x, local_n, MPI_INT, x, local_n, MPI_INT, comm);
-	//for(i=0; i<n; ++i)
-		//printf("Proceso %d, tengx %d\n", rank,x[i]);
-	
-	for(i=0; i<local_m; ++i){
-		
-		local_y[i]=0;
-		for(j=0; j<n; ++j){
-			local_y[i]+=local_A[i*n+j]*x[j];
-			//printf("Proceso %d, opero %d x %d\n", rank,local_A[i*n+j],x[j]);
-		}
-	}
-	free(x);
-
-	MPI_Gather(local_y, local_m, MPI_INT, res, local_m, MPI_INT, 0,
-           MPI_COMM_WORLD);
-	local_finish=MPI_Wtime();
-	local_elapsed=local_finish-local_start;
-	MPI_Reduce(&local_elapsed, &elapsed, 1, MPI_DOUBLE, MPI_MAX, 0, comm);
-	if(rank==0){	
-		
-		for(i=0; i<m; ++i){
-			//printf("%d, ",res[i]);
-		}
-		printf("Elapsed time = %f seconds\n", elapsed);
-	}
-
-	MPI_Finalize();	
-
-	return 0;
 }
+
+void Print_vector(double local_b[], int local_n, int n, char title[], int my_rank, MPI_Comm comm){
+  double* b = NULL;
+  int i;
+  if(my_rank == 0){
+    b = malloc(n*sizeof(double));
+    MPI_Gather(local_b, local_n, MPI_DOUBLE,b,local_n, MPI_DOUBLE,0,comm);
+    printf("%s\n", title);
+    for(i = 0; i < n; i++){
+      printf("%f ", b[i]);
+    }
+    printf("\n");
+    free(b);
+  }
+  else{
+    MPI_Gather(local_b, local_n, MPI_DOUBLE, b, local_n, MPI_DOUBLE, 0, comm);
+  }
+}
+
+//La funcion hace uso de Allgather: Se encarga que la matriz X, con las que todas las filas de A mult esté en cada proceso ,
+//Como una Matriz global
+void Mat_vect_mult(double local_A[], double local_x[], double local_y[], int local_m, int n, int local_n, MPI_Comm comm){
+  double* x;
+  int local_i,j;
+  int local_ok=1;
+  x = malloc(n*sizeof(double));
+  MPI_Allgather(local_x, local_n, MPI_DOUBLE, x, local_n, MPI_DOUBLE, comm);
+  for(local_i = 0; local_i < local_m; local_i++){
+    local_y[local_i] = 0.0;
+    for(j = 0; j <n; j++){
+      local_y[local_i] += local_A[local_i*n+j] * x[j];
+    }
+  }
+  free(x);
+}
+
+int main(){
+  int comm_sz;
+  int my_rank;
+  double start,finish;
+  //m filas n columnas , al compilar -n a; a = num de filas en este caso 4,
+  //m y n deben ser multiplos o iguales.
+  int m=4;
+  int n= 4,local_n,local_m;
+  MPI_Init(NULL,NULL);
+  MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
+  MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+  local_n = n/comm_sz; /*Divide Matriz en bloquesitos para dárselos a los n procesos*/
+  local_m = m/comm_sz; 
+  double local_x[local_n],local_y[local_m],local_A[local_n*local_m];
+  Read_vector(local_A,local_n+local_m,n*m,"A",my_rank,MPI_COMM_WORLD);
+  Read_vector(local_x,local_n,n,"X",my_rank,MPI_COMM_WORLD);
+  Mat_vect_mult(local_A, local_x, local_y, local_m,n,local_n,MPI_COMM_WORLD);
+  Print_vector(local_y,local_m,m,"Y", my_rank,MPI_COMM_WORLD);
+
+  MPI_Finalize();
+  return 0;
+}
+
